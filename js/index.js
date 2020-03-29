274eaae9d20b54afeb122262328ae1307b9b8fce
Ari Johannesson <arividar@gmail.com>
2020-03-29 19:09:35 +0000

    Adds module reference to index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 "use strict";
+
+const {
+    Game,
+    Ball,
+    Paddle,
+    GameElement
+  } = require("./pong");
+
 let canvas = document.getElementById("skjarinn");
 let ctx = canvas.getContext("2d");
