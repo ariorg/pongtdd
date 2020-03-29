97b731e89c18450c1870e4764404763ed301c3e3
Ari Johannesson <arividar@gmail.com>
2020-03-29 00:37:43 +0000

    No changes - reformats only

--- js/pong.test.js
+++ js/pong.test.js
@@ -1,64 +1,64 @@
-const { PongGame, PongBall, Sprite } = require('./pong')
+const { PongGame, PongBall, Sprite } = require("./pong");
 
-describe('PongGame', () => {
-    test('Create pong game', () => {
-        let pg = new PongGame();
-        expect(typeof pg != "undefined").toBeTruthy();
-    });
+describe("PongGame", () => {
+  test("Create pong game", () => {
+    let pg = new PongGame();
+    expect(typeof pg != "undefined").toBeTruthy();
+  });
 
-    test('New PongGame has default sized canvas', () => {
-        const pg = new PongGame();
-        expect(pg.CanvasWidth).toBeGreaterThan(0);
-        expect(pg.CanvasHeight).toBeGreaterThan(0);
-    });
+  test("New PongGame has default sized canvas", () => {
+    const pg = new PongGame();
+    expect(pg.CanvasWidth).toBeGreaterThan(0);
+    expect(pg.CanvasHeight).toBeGreaterThan(0);
+  });
 
-    test('Create PongGame with nondefault canvas size', () => {
-        const pg1 = new PongGame();
-        const w = pg1.CanvasWidth+15;
-        const h = pg1.CanvasHeight+15;
-        const pg = new PongGame(w, h);
-        expect(pg.CanvasWidth).toBe(w);
-        expect(pg.CanvasHeight).toBe(h);
-    });
+  test("Create PongGame with nondefault canvas size", () => {
+    const pg1 = new PongGame();
+    const w = pg1.CanvasWidth + 15;
+    const h = pg1.CanvasHeight + 15;
+    const pg = new PongGame(w, h);
+    expect(pg.CanvasWidth).toBe(w);
+    expect(pg.CanvasHeight).toBe(h);
+  });
 
-    test('Game should have an initial score of zero', () => {
-        const g = new PongGame();
-        expect(g.Score).toBe(0);
-    });
+  test("Game should have an initial score of zero", () => {
+    const g = new PongGame();
+    expect(g.Score).toBe(0);
+  });
 });
 
 // Sprite tests
-describe('Sprite', () => {
-    test('should have x,y coordinates and diameter equal to zero after creation', () => {
-        const s = new Sprite();
-        expect(s.X).toBe(0);
-        expect(s.Y).toBe(0);
-        expect(s.Diameter).toBe(0);
-    });    
+describe("Sprite", () => {
+  test("should have x,y coordinates and diameter equal to zero after creation", () => {
+    const s = new Sprite();
+    expect(s.X).toBe(0);
+    expect(s.Y).toBe(0);
+    expect(s.Diameter).toBe(0);
+  });
 });
 
 // Ball tests -------------------------
 
-describe('PongBall', () => {
-    test("Create new PongBall should have coords 0,0 and diameter > 0", () => {
-        const pb = new PongBall();
-        expect(pb.X).toBe(0);
-        expect(pb.Y).toBe(0);
-        expect(pb.Diameter).toBeGreaterThan(0);
-    });
+describe("PongBall", () => {
+  test("Create new PongBall should have coords 0,0 and diameter > 0", () => {
+    const pb = new PongBall();
+    expect(pb.X).toBe(0);
+    expect(pb.Y).toBe(0);
+    expect(pb.Diameter).toBeGreaterThan(0);
+  });
 
-    test("Set ball coordinates", () => {
-        const ball = new PongBall();
-        ball.X = 40;
-        ball.Y = 20;
-        expect(ball.X).toBe(40);
-        expect(ball.Y).toBe(20);
-    });
+  test("Set ball coordinates", () => {
+    const ball = new PongBall();
+    ball.X = 40;
+    ball.Y = 20;
+    expect(ball.X).toBe(40);
+    expect(ball.Y).toBe(20);
+  });
 
-    test("Set ball diameter to x should be x", () => {
-        const ball = new PongBall();
-        const newD = ball.Diameter+5;
-        ball.Diameter = newD;
-        expect(ball.Diameter).toBe(newD);
-    });
+  test("Set ball diameter to x should be x", () => {
+    const ball = new PongBall();
+    const newD = ball.Diameter + 5;
+    ball.Diameter = newD;
+    expect(ball.Diameter).toBe(newD);
+  });
 });
