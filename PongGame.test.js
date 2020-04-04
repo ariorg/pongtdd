c1257a3f523269884231efa306f296bff4419541
Ari Johannesson <arividar@gmail.com>
2020-04-04 04:49:25 +0000

    Just some formatting

--- PongGame.test.js
+++ PongGame.test.js
@@ -4,29 +4,31 @@ import 'jest-canvas-mock';
 import PongGame from "./PongGame";
 
 describe("PongGame", () => {
-  test("Create pong game", () => {
-    let pg = new PongGame(document.createElement("canvas"));
-    expect(typeof pg != "undefined").toBeTruthy();
-    console.log(typeof pg);
-  });
+  describe("PongGame constructor", () => {
+    test("Create pong game", () => {
+      let pg = new PongGame(document.createElement("canvas"));
+      expect(typeof pg != "undefined").toBeTruthy();
+      console.log(typeof pg);
+    });
 
-  test("New Game has default sized canvas", () => {
-    const pg = new PongGame(document.createElement("canvas"));
-    expect(pg.CanvasWidth).toBeGreaterThan(0);
-    expect(pg.CanvasHeight).toBeGreaterThan(0);
-  });
+    test("New Game has default sized canvas", () => {
+      const pg = new PongGame(document.createElement("canvas"));
+      expect(pg.CanvasWidth).toBeGreaterThan(0);
+      expect(pg.CanvasHeight).toBeGreaterThan(0);
+    });
 
-  test("Width and height of passed in canvas is same in Game object", () => {
-    const canvas = document.createElement("canvas");
-    canvas.width = 801;
-    canvas.height = 601;
-    const pg = new PongGame(canvas);
-    expect(pg.CanvasWidth).toBe(801);
-    expect(pg.CanvasHeight).toBe(601);
-  });
+    test("Width and height of passed in canvas is same in Game object", () => {
+      const canvas = document.createElement("canvas");
+      canvas.width = 801;
+      canvas.height = 601;
+      const pg = new PongGame(canvas);
+      expect(pg.CanvasWidth).toBe(801);
+      expect(pg.CanvasHeight).toBe(601);
+    });
 
-  test("Game should have an initial score of zero", () => {
-    const g = new PongGame(document.createElement("canvas"));
-    expect(g.Score).toBe(0);
+    test("Game should have an initial score of zero", () => {
+      const g = new PongGame(document.createElement("canvas"));
+      expect(g.Score).toBe(0);
+    });
   });
 });
--- index.js
+++ index.js
@@ -5,3 +5,4 @@ import PongGame from "./PongGame.js";
 let canvas = document.getElementById("skjarinn");
 let ctx = canvas.getContext("2d");
 let pongGame = new PongGame(ctx);
+pongGame.draw();
