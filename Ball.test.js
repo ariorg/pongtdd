0f2e76940111430c01b6a408efa823cc9d8090f5
Ari Johannesson <arividar@gmail.com>
2020-04-19 11:39:22 +0000

    Adds test for random location of ball when game starts

--- Ball.test.js
+++ Ball.test.js
@@ -1,5 +1,6 @@
 'use strict';
-import Ball from './Ball';
+import Ball from './Ball.js';
+import Paddle from './Paddle.js';
 
 describe("Ball class tests", () => {
     const ctx = document.createElement("canvas").getContext('2d');
@@ -33,7 +34,24 @@ describe("Ball class tests", () => {
 
     test("Diameter cannot be an even number and must be an integer", () => {
         const ball = new Ball(ctx);
-        expect(() => { ball.Diameter = 10;   }).toThrow();
+        expect(() => { ball.Diameter = 10; }).toThrow();
         expect(() => { ball.Diameter = 9.01; }).toThrow();
+    });
+
+    test("startNewGame ball max/min random location must match edges of the paddle", () => {
+        const ball = new Ball(ctx, 0, 0, 5);
+        const paddle = new Paddle(ctx, 15, 5);
+        paddle.startNewGame();
+
+        const mockMath = Object.create(global.Math);
+        global.Math = mockMath;
+
+        mockMath.random = () => 0.99999999;
+        ball.startNewGame(paddle);
+        expect(ball.RightX).toBe(paddle.RightX);
+        
+        mockMath.random = () => 0.00000001;
+        ball.startNewGame(paddle);
+        expect(ball.LeftX).toBe(paddle.LeftX);
     });
 });
