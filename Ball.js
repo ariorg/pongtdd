725ffcae85f267c37c72c260ca71d7b49370a9d4
Ari Johannesson <arividar@gmail.com>
2020-04-21 07:44:07 +0000

    Fix radius rendering of ball

--- Ball.js
+++ Ball.js
@@ -28,7 +28,7 @@ export default class Ball extends GameElement {
 
     draw() {
         this._ctx.beginPath();
-        this._ctx.arc(this.X, this.Y, this.Radius, 0, Math.PI * 2);
+        this._ctx.arc(this.X, this.Y, this.Radius + 1, 0, Math.PI * 2);
         this._ctx.fillStyle = "blue";
         this._ctx.fill();
         this._ctx.closePath()
