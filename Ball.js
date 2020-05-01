fa2402d7e76db8b3a6e42e3296b783598fe209e3
Ari Johannesson <arividar@gmail.com>
2020-05-01 14:34:02 +0000

    Minor reformat

--- Ball.js
+++ Ball.js
@@ -46,14 +46,11 @@ export default class Ball extends MovingGameElement {
     _detectCollisionAndSetDirection(paddle) {
         if (this.YBottom >= paddle.YTop - 1 && this.XRight >= paddle.XLeft && this.XLeft <= paddle.XRight)
             this.YDirection = -1;
-        if (this.YTop <= 0)
-            this.YDirection = 1;
-        if (this.YBottom >= this._ctx.canvas.height - 1)
-            this.YDirection = -1;
-        if (this.XLeft <= 0)
-            this.XDirection = 1;
-        if (this.XRight >= this._ctx.canvas.width - 1)
-            this.XDirection = -1;
-    }
 
+        if (this.YTop <= 0) this.YDirection = 1;
+        if (this.YBottom >= this._ctx.canvas.height - 1) this.YDirection = -1;
+
+        if (this.XLeft <= 0) this.XDirection = 1;
+        if (this.XRight >= this._ctx.canvas.width - 1) this.XDirection = -1;
+    }
 }
