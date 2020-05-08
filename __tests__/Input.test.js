1b4c02c9dc1fbf3dcce4a07296f9a4a1586b2aea
Ari Johannesson <arividar@gmail.com>
2020-05-08 08:00:08 +0000

    Adds failing test for Input

--- __tests__/Input.test.js
+++ __tests__/Input.test.js
@@ -1,12 +1,21 @@
 import Input from '../js/Input.js'
 
-describe("Input class tests", () => {
-  describe("constructor", () => {
-    test("Input creation", () => {
-      const input = new Input();
-      expect(input).toBeDefined();
-      expect(input.IsLeftKeyPressed).toBeFalsy();
-      expect(input.IsRightKeyPressed).toBeFalsy();
-    });
-  });
-});
+describe('Input class tests', () => {
+    describe('constructor', () => {
+        test('Input creation', () => {
+            const input = new Input()
+            expect(input).toBeDefined()
+            expect(input.IsLeftKeyPressed).toBeFalsy()
+            expect(input.IsRightKeyPressed).toBeFalsy()
+        });
+
+        test('Input creation should add input-handlers to document', () => {
+            let spyOnAddEventListener = jest.spyOn(
+                thisGlobal.document,
+                'addEventListener'
+            );
+            const input = new Input()
+            expect(spyOnAddEventListener).toHaveBeenCalledTimes(3);
+        });
+    })
+})
