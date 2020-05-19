ac4fca33f93c3437491be32d4febc60296764782
Ari Johannesson <arividar@gmail.com>
2020-05-19 22:50:55 +0000

    Starts a test for PlaySound method

--- __tests__/SoundPlayer.test.js
+++ __tests__/SoundPlayer.test.js
@@ -1,8 +1,18 @@
 import SoundPlayer from '../js/SoundPlayer.js';
 
-describe('constructor', () => {
+describe('SoundPlayer tests', () => {
+  describe('constructor', () => {
     it('should create a SoundPlayer', () => {
-        const sp = new SoundPlayer();
-        expect(sp).toBeDefined();
+      const sp = new SoundPlayer();
+      expect(sp).toBeDefined();
     });
+  });
+
+  describe('PlaySound method', () => {
+    it.skip('should play sound', () => {
+      const sp = new SoundPlayer();
+      // Todo: Mock soundplayer - see here:
+      //       https://jestjs.io/docs/en/es6-class-mocks
+    });
+  });
 });
