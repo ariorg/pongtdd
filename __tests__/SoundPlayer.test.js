1fc3f0e9cbc08dfb3b4d501537a57edb52927a05
Ari Johannesson <arividar@gmail.com>
2020-05-21 21:15:56 +0000

    Test and implement soundplayer

--- __tests__/SoundPlayer.test.js
+++ __tests__/SoundPlayer.test.js
@@ -5,13 +5,17 @@ describe('SoundPlayer tests', () => {
     it('should create a SoundPlayer', () => {
       const sp = new SoundPlayer();
       expect(sp).toBeDefined();
+      expect(sp.Volume).toBe(1.0);
     });
   });
 
   describe('PlaySound method', () => {
-    afterEach(() => { jest.clearAllMocks(); });
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
 
     it('should play sound', () => {
+      window.HTMLMediaElement.prototype.play = () => {};
       const sp = new SoundPlayer();
       const playSpy = jest.spyOn(sp, 'play');
       const soundFileName = 'sound.wav';
--- js/SoundPlayer.js
+++ js/SoundPlayer.js
@@ -1,6 +1,15 @@
 export default class SoundPlayer {
-  constructor() {
+  constructor(volume) {
+    this.Volume = volume || 1.0;
   }
+  
+  set Volume(value) { this._volume = value; }
+  get Volume() { return this._volume; }
 
-  play() {}
+  play(soundFileName, volume) {
+    if (volume) this.Volume = volume;
+    const snd = new Audio(soundFileName);
+    snd.volume = this.Volume;
+    snd.play();
+  }
 }
