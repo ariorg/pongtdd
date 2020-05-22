c62f4661bfc78a08c82af015e56f9043118072ab
Ari Johannesson <arividar@gmail.com>
2020-05-22 18:30:36 +0000

    Adds class PongSounds

---
+++ js/PongSounds.js
@@ -0,0 +1,9 @@
+export default class PongSounds {
+  constructor() {}
+}
+
+const PongSound = {
+  COLLISION_BALL_WALL: 'file.wav',
+  COLLISION_BALL_PADDLE: 'file2.mp3',
+  GAME_OVER: 'file3.wav',
+};
