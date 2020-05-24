aeef8def51f3897207c0b5465c4b17d67a4d92fc
Ari Johannesson <arividar@gmail.com>
2020-05-24 09:57:38 +0000

    Refactor tests

--- __tests__/BallCollisionSounds.test.js
+++ __tests__/BallCollisionSounds.test.js
@@ -12,31 +12,35 @@ describe('Ball collision sound tests', () => {
   });
 
   describe('Ball collision sounds', () => {
-    it('collision within the paddle should play sound', () => {
+    function _newBall() {
       const ctx = document.createElement('canvas').getContext('2d');
       ctx.canvas.width = 800;
       ctx.canvas.height = 600;
       const ball = new Ball(ctx, 5);
+      ball.Speed = 1;
+      ball.YDirection = 1;
+      return ball;
+    }
+
+    function _newPaddle(ball) {
       const paddle = new Paddle(ball._ctx, new Input(), 121, 15);
       paddle.resetGame();
       paddle.Y = 100;
       paddle.X = 50;
-      ball.YDirection = 1;
+      return paddle;
+    }
+
+    it('collision with the paddle should play sound', () => {
+      const ball = _newBall();
+      const paddle = _newPaddle(ball);
       ball.YBottom = paddle.YTop - 1;
       ball.update(paddle);
       expect(PongSounds.mock.instances[0].ballCollidesWithPaddle).toHaveBeenCalledTimes(1);
     });
-    it('no collision within the paddle should not play sound', () => {
-      const ctx = document.createElement('canvas').getContext('2d');
-      ctx.canvas.width = 800;
-      ctx.canvas.height = 600;
-      const ball = new Ball(ctx, 5);
-      const paddle = new Paddle(ball._ctx, new Input(), 121, 15);
-      paddle.resetGame();
-      paddle.Y = 100;
-      paddle.X = 50;
-      ball.YDirection = 1;
-      ball.Speed = 1;
+
+    it('not colliding with paddle should not play sound', () => {
+      const ball = _newBall();
+      const paddle = _newPaddle(ball);
       ball.YBottom = paddle.YTop - 10;
       ball.update(paddle);
       expect(PongSounds.mock.instances[0].ballCollidesWithPaddle).toHaveBeenCalledTimes(0);
